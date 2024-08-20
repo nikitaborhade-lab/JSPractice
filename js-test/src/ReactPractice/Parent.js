66e0a021bd875cb41fc2a36384f3b62b49574905
Nikita Borhade <78292956+nikitaborhade-lab@users.noreply.github.com>
2024-08-20 06:02:22 +0000

    JS and React Code Practice

--- js-test/src/ReactPractice/Parent.js
+++ js-test/src/ReactPractice/Parent.js
@@ -1,13 +1,45 @@
 import Child from './Child'
+import { createContext } from 'react'
+// import React, {useState } from 'react'
+
+// Passing data from parent component to child component using ***props***
+
+// export default function Parent(){
+//     return(
+//         <>
+//         <h2>Parent to Child</h2>
+//         <Child message = {'Message from Parent to Child using props'} />
+//         <div>-------------------------------------------------------------</div>
+//         </>
+//     )
+// }
+
+// Using ***state***
+
+// export default function Parent(){
+//     const[state, setState] = useState("message from parent using state");
+//     return(
+//         <>
+//         <h2>Parent to Child</h2>
+//         <Child message= {state}/>
+//         <div>-------------------------------------------------------------</div>
+//         </>
+//     )
+// }
+
+// Using ***context***
+
+export const MyContext = createContext();
 
 export default function Parent(){
+    const data = "message from parent using context";
     return(
         <>
-        <h2>Parent to Child</h2>
-        <Child message = {'Message from Parent to Child'} />
+        <h1>Parent to Child</h1>
+        <MyContext.Provider value={data}>
+            <Child />
+        </MyContext.Provider>
         <div>-------------------------------------------------------------</div>
         </>
     )
 }
-
-// Passing data from parent component to child component
