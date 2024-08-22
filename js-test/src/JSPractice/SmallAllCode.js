63637a19a79169709963f04b7188014ea12fd284
Nikita Borhade <78292956+nikitaborhade-lab@users.noreply.github.com>
2024-08-22 14:20:48 +0000

    JS Code

--- js-test/src/JSPractice/SmallAllCode.js
+++ js-test/src/JSPractice/SmallAllCode.js
@@ -1,23 +1,21 @@
 // 1) convert into uppercase
-
-// export default function SmallAllCode(){
-//     let string = "nikita";
-//     const sample = string.toUpperCase();
-//     console.log(sample);
-// }
-
 // 2) convert into lowercase
-
-// export default function SmallAllCode(){
-//     let string = "NIKITA";
-//     const sample = string.toLowerCase();
-//     console.log(sample);
-// }
-
 // 3) First letter uppercase
 
 export default function SmallAllCode(){
     let string = "nikita";
-    const sample = string.charAt(0).toUpperCase() + string.slice(1);
+    let string1 = "BORHADE";
+    const sample = string.toUpperCase();
+       const sample1 = string1.toLowerCase();
+       const sample2 = string.charAt(0).toUpperCase() + string.slice(1);
     console.log(sample);
+       console.log(sample1);
+       console.log(sample2);
+
+       let sum = 5 + 10;
+   debugger; // Execution will pause here
+   return sum;
 }
+
+
+ 
