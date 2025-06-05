d52df96e25ebeb93539113f7873ed92fcab2293d
Nikita Borhade <78292956+nikitaborhade-lab@users.noreply.github.com>
2025-06-05 11:49:01 +0530

    Update LargestNo.js

--- js-test/src/JSPractice/LargestNo.js
+++ js-test/src/JSPractice/LargestNo.js
@@ -23,3 +23,14 @@ export default function LargestNo(){
 //     }
 //     console.log("Smallest Number", Smallest)
 // }
+
+// find 2 elements from array whose addition is 9
+const arr  = [2,1,4,5,6];
+
+for(let i=0; i<arr.length; i++){
+    for(let j=i+1; j<arr.length; j++){
+        if(arr[i] + arr[j] === 9){
+            console.log(`${arr[i]} + ${arr[j]} = 9`);
+        }
+    }
+}
